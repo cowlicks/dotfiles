25947fac1e3579206dd5c851bc46b171bfd92169
Blake <blake.a.griffith@gmail.com>
2012-12-25 14:34:37 -0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,27 @@
-#! /bin/bash
+#! /usr/bin/env bash
 # Script for deploying dot files.
 
 # TODO
 
 # Set home dir and old_dotfiles dir.
+dir=~/dotfiles
+olddir=~/dotfiles_old
+files="bashrc irssi tmux.conf vimrc"
 
 # mv existing dotfiles to old_dotfiles
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "done"
 
 # link my dotfiles to the home dir.
+echo "Changing to the $dir directory"
+cd $dir
+echo "done"
+
+#Move dotfiles into the homedir and existing dotfiles to dotfiles_old dir.
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home dir."
+    ln -s $dir/$file ~/.$file
+done
