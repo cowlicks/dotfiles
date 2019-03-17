ede0cba6a33cda098a9573dc31d9c3dab0574a32
Blake Griffith <blake.a.griffith@gmail.com>
2019-03-17 14:58:23 -0700

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,21 @@
 #! /usr/bin/env bash
 # Script for deploying dot files.
 
-# TODO
+set -e
 
-# Set home dir and old_dotfiles dir.
-dir=~/.dotfiles
-olddir=~/dotfiles_old
-files="bashrc condarc irssi tmux.conf vim vimrc global_gitignore gitconfig zshrc"
+dot_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+olddir=~/.dotfiles_old
+files="bashrc tmux.conf vim vimrc global_gitignore gitconfig zshrc"
 
 # mv existing dotfiles to old_dotfiles
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
 echo "done"
 
-# link my dotfiles to the home dir.
-echo "Changing to the $dir directory"
-cd $dir
-echo "done"
-
 #Move dotfiles into the homedir and existing dotfiles to dotfiles_old dir.
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    mv ~/.$file $olddir/ || true
     echo "Creating symlink to $file in home dir."
-    ln -s $dir/$file ~/.$file
+    ln -s $dot_dir/$file ~/.$file
 done
